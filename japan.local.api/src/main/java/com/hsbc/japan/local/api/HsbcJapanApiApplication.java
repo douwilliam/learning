a8bdf6a5e83c61ed505b0ddea1057ca324d350c2
douwilliam <douwilliam@users.noreply.github.com>
2024-05-13 02:16:47 +0900

    add api platform

---
+++ japan.local.api/src/main/java/com/hsbc/japan/local/api/HsbcJapanApiApplication.java
@@ -0,0 +1,13 @@
+package com.hsbc.japan.local.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HsbcJapanApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HsbcJapanApiApplication.class, args);
+	}
+
+}
---
+++ japan.local.api/src/test/java/com/hsbc/japan/local/api/HsbcJapanApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hsbc.japan.local.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HsbcJapanApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
